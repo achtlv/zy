7f73d5368034a838ffbf1b1c143124b18d1dd6ba
ddz-deploy <deploy@localhost>
2026-08-19 14:04:36 +0800

    chore: 移除调试脚本

--- find-mz.js
+++
@@ -1,5 +0,0 @@
-const s = require('fs').readFileSync('ddz/static/js/app.03832cca.chunk.js', 'utf8');
-// M.Z 大厅组件（桌游列表删除后的残留）——找 notice 相关结构
-let i = s.indexOf('t&&(n?(0,f.jsx)(o.Z,{noStyle:!0,to:n,className:"whitespace-pre-line');
-console.log('notice block @', i);
-if (i > 0) console.log(s.slice(i - 200, i + 900));
--- verify3.js
+++
@@ -1,7 +0,0 @@
-const s = require('fs').readFileSync('ddz/static/js/app.03832cca.chunk.js', 'utf8');
-const esc = (str) => str.replace(/[\u4e00-\u9fa5]/g, (c) => '\\u' + c.charCodeAt(0).toString(16).padStart(4, '0'));
-for (const str of ['公告', '查看规则', '视频规则', '公众号', '微信登录', 'QQ 登录', '京ICP', '京公网安备', '桌游列表']) {
-  const i = s.indexOf(esc(str));
-  console.log(str, '->', i);
-  if (i > 0) console.log('   ctx:', s.slice(Math.max(0, i - 80), i + 80).slice(0, 160));
-}
